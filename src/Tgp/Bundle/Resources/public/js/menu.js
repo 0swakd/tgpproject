2a55ee51155cad6d7b6eb4e91ef7820bd66be48b
0swakd <antoine.bascou@gmail.com>
2015-11-11 15:28:48 +0100

    dummy menu

--- src/Tgp/Bundle/Resources/public/js/menu.js
+++ src/Tgp/Bundle/Resources/public/js/menu.js
@@ -13,11 +13,15 @@ function openGlobalItem (e) {
     return;
 }
 function openSeekItem () {
-    show(document.getElementById("seek_place_box"));
+    show(document.getElementById("seek_box"));
     return;
 }
 function openSeekPlaceItem () {
-    show(document.getElementById("seek_box"));
+    show(document.getElementById("seek_place_box"));
+    return;
+}
+function openGeolocItem () {
+    show(document.getElementById("geoloc_box"));
     return;
 }
 function openFriendsItem () {
@@ -40,6 +44,10 @@ function closeSeekPlaceItem () {
     hide(document.getElementById("seek_place_box"));
     return;
 }
+function closeGeolocItem () {
+    hide(document.getElementById("geoloc_box"));
+    return;
+}
 function closeFriendsItem () {
     hide(document.getElementById("friends_list"));
     return;
@@ -58,7 +66,8 @@ function initMenu() {
     menuRight = menuRight || new ItemMenu();
     menuRight.addItem('menu_title', openGlobalItem, closeGlobalItem);
     menuRight.addItem('seek_place_title', openSeekPlaceItem, closeSeekPlaceItem);
-    menuRight.addItem('seek_title', openSeekItem, closeSeekPlaceItem);
+    menuRight.addItem('geoloc_title', openGeolocItem, closeGeolocItem);
+    menuRight.addItem('seek_title', openSeekItem, closeSeekItem);
     menuRight.addItem('friends_title', openFriendsItem, closeFriendsItem);
     menuRight.addItem('places_title', openPlacesItem, closePlacesItem);
     updateMenu();
